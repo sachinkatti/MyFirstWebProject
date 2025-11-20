0f4442ef01d54cdf6f387a898c7db54983be4829
sachinkatti <sachinkatti19@gmail.com>
2025-11-20 14:37:23 +0530

    Add unique key props to list children in App.js

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 /* 
 
 <div id="parent">
@@ -16,11 +19,11 @@ ReactElement(Object) => HTML(Browser Understands)
 */
 
 const parent = React.createElement('div', { id: 'parent' }, [
-  React.createElement('div', { id: 'child' }, [
-    React.createElement('h1', {}, "I'm h1 Tag"),
+  React.createElement('div', { id: 'child', key: 'child' }, [
+    React.createElement('h1', {}, 'This is Namaste React 🚀'),
     React.createElement('h2', {}, "I'm h2 Tag"),
   ]),
-  React.createElement('div', { id: 'child2' }, [
+  React.createElement('div', { id: 'child2', key: 'child2' }, [
     React.createElement('h1', {}, "I'm h1 Tag"),
     React.createElement('h2', {}, "I'm h2 Tag"),
   ]),
